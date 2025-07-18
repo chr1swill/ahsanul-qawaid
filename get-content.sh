b791d64cbe7a03a4b4f167c0a4c86c1b107039df
chr1swill <alexanderwilliams20003@gmail.com>
2025-07-18 15:51:30 -0400

    refactor: reoganizing files

---
+++ get-content.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#set -xe
+
+root=$HOME/code/projects/ahsanul-qawaid
+
+n_pages=56
+website=https://myelm.co.uk/aq-new/
+
+# page 1 has no audio files
+# for (( i=2; i<="$n_pages"; ++i ));
+# do
+#   echo $i
+# done
+# we don't need all the pages audio files since
+# alot of the first pages have the same content (alif to ya)
