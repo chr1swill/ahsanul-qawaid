b384885a0e96b1faf054e9ca89b03f7b5f3f71a9
chr1swill <alexanderwilliams20003@gmail.com>
2025-07-18 16:44:46 -0400

    feat: create a page to get content for pages that match that schema - still not tested and will not work for some pages

--- get-content.sh
+++ get-content.sh
@@ -2,15 +2,56 @@
 
 #set -xe
 
-root=$HOME/code/projects/ahsanul-qawaid
+PAGE_MIN=1
+PAGE_MAX=56
 
-n_pages=56
+root_dir=$HOME/code/projects/ahsanul-qawaid/
+audio_dir=${root_dir}audio/
 website=https://myelm.co.uk/aq-new/
 
-# page 1 has no audio files
-# for (( i=2; i<="$n_pages"; ++i ));
-# do
-#   echo $i
-# done
-# we don't need all the pages audio files since
-# alot of the first pages have the same content (alif to ya)
+page_number="$1"
+if [ "$page_number" == "" ];
+then
+  echo "parameter 1 empty: expected page number" && exit 1
+elif [ "$page_number" -lt "$PAGE_MIN" ];
+then
+  echo "parameter 1 less than ${PAGE_MIN}: expected number greater then or equal to ${PAGE_MIN}"  && exit 2
+elif [ "$page_number" -gt "$PAGE_MAX" ];
+then
+  echo "parameter 1 greater than ${PAGE_MAX}: expected number less than or equal to ${PAGE_MIN}"  && exit 3
+fi
+
+page_dir="${audio_dir}${page_number}/"
+if [ ! -d "${page_dir}" ];
+then
+  mkdir -p "$page_dir"
+fi
+
+page_path=""
+if [ "$page_number" -lt "10" ];
+then
+  page_path="_00${page_number}/"
+else
+  page_path="_0${page_number}/"
+fi
+
+# TODO: max iters 100 is just a arbitrary number 
+# probably should fact check 
+# to not waste time with unnessary requests
+for (( i=1; i<100; ++i ));
+do
+  local file;
+
+  if [ "$i" -lt "10" ];
+  then
+    file="_0${page_number}"
+  else
+    file="_${page_number}"
+  fi
+
+  curl "${website}${page_path}${file}.mp3" -o "${page_dir}${i}.mp3" &
+done
+
+wait
+
+exit 0
