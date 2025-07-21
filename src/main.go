dd1b195a7e4f87b813724a41abe5fa621312dd03
chr1swill <alexanderwilliams20003@gmail.com>
2025-07-21 18:50:58 -0400

    feat: ensure whitespace is removed around slots - fix bug where .Vowel and .Name were out of scope - fix bug where $idx was called as function instead of lt in if statement

--- src/main.go
+++ src/main.go
@@ -1,7 +1,7 @@
 package main
 
 import  (
-  "net/http"
+  "os"
 	"html/template"
 )
 // hello
@@ -28,42 +28,48 @@ type Section struct {
 
 func main() {
 	var err error;
-	var mux *http.ServeMux;
-	var t *template.Template;
+	//var mux *http.ServeMux;
+	//var t *template.Template;
 	var sections []Section;
-
-	t = template.Must(template.New("index.html").Funcs(template.FuncMap{
-		"Add": func (a, b int) int { return a + b }, 
-	}).ParseFiles("src/html/index.html"));
 
 	sections = append(sections, 
 		Section{Letters: ARA_ALPHA, Name: "1", Vowel: ""});
-	// sections = append(sections, 
-	// 	Section{Letters: ARA_ALPHA, Name: "11", Vowel: ""});
+	sections = append(sections, 
+		Section{Letters: ARA_ALPHA, Name: "11", Vowel: "َ",
+    Words: []string{"اَمَرَ", "بَلَغَ", "ثَمَرَ", "جَمَعَ", "حَسَدَ", "ذَكَرَ",
+    "رَفَعَ", "زَعَمَ", "سَرَقَ", "صَدَقَ", "ضَرَبَ", "ظَلَمَ", "عَدَلَ", "قَمَرَ",
+    "كَسَبَ", "وَجَدَ"}});
+	sections = append(sections, 
+		Section{Letters: ARA_ALPHA, Name: "11", Vowel: "َ",
+    Words: []string{"اَذِنَ", "بَقِيَ", "حَمِدَ", "خَشِيَ", "سَخِرَ", "شَرِبَ",
+    "عَجِبَ", "غَضِبَ", "كَذِبَ", "بَخِلَ", "فَلِمَ", "يَءِسَ", "اَبَتِ",
+    "بَلَدِ", "مَلِكِ", "حَطَبِ", "اِبِلِ", "كِبَرِ", "عَلِمَ", "اِرَمَ"}});
 
-	// err = t.Execute(os.Stdout, sections);
+	err = template.Must(template.New("index.html").Funcs(template.FuncMap{
+		"Add": func (a, b int) int { return a + b }, 
+	}).ParseFiles("src/html/index.html")).Execute(os.Stdout, sections);
+  blowup_if_present(err);
+
+	// mux = http.NewServeMux();
+
+	// mux.Handle("/audio/",
+	// 	http.StripPrefix("/audio/",
+	// 		http.FileServer(http.Dir("audio"))));
+
+	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	if r.Method != http.MethodGet {
+	// 		http.Error(w,
+	// 		"Method not allowed", http.StatusMethodNotAllowed);
+	// 	}
+
+	// 	err = t.Execute(w, sections);
+	// 	if err != nil {
+	// 		http.Error(w,
+	// 		"Internal Server Error", http.StatusInternalServerError);
+	// 	}
+	// });
+
+	// print("server running on port", PORT);
+	// err = http.ListenAndServe(PORT, mux);
 	// blowup_if_present(err);
-
-	mux = http.NewServeMux();
-
-	mux.Handle("/audio/",
-		http.StripPrefix("/audio/",
-			http.FileServer(http.Dir("audio"))));
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
-			http.Error(w,
-			"Method not allowed", http.StatusMethodNotAllowed);
-		}
-
-		err = t.Execute(w, sections);
-		if err != nil {
-			http.Error(w,
-			"Internal Server Error", http.StatusInternalServerError);
-		}
-	});
-
-	print("server running on port", PORT);
-	err = http.ListenAndServe(PORT, mux);
-	blowup_if_present(err);
 }
