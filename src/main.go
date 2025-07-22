1caea3cc6d4588fbb01162dd00924eb35d4a1e12
chr1swill <alexanderwilliams20003@gmail.com>
2025-07-22 11:07:18 -0400

    feat: remove comments

--- src/main.go
+++ src/main.go
@@ -4,10 +4,6 @@ import  (
   "net/http"
 	"html/template"
 )
-// hello
-// my limits are the ammount of data i can store on the server (10GB)
-// the badwidth of the server (1GB/s)
-// the ammount of memeory I can use (1GB)
 
 const (
 	PORT = ":8080"
